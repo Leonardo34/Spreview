034aa6f472ea59fdcf6cba4f81af8e6a44f2b1fd
Leonardo Morais <leonardomorais@MacBook-Pro-de-Leonardo.local>
2017-11-25 16:09:51 -0200

    init aplication

--- app/src/main/java/com/example/leonardomorais/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/leonardomorais/myapplication/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.leonardomorais.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private val textView by lazy { findViewById<TextView>(R.id.text_view) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView.text = "bomba"
     }
 }
